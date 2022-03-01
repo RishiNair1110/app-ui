7ac0e98c054909550b20385348f0d34e348cb13b
RishiNair1110 <Nairrishi2001@gmail.com>
2022-03-01 15:23:47 +0300

    Final code

--- app-ui/src/BasicConnection.jsx
+++ app-ui/src/BasicConnection.jsx
@@ -7,11 +7,12 @@ import "./app.css";
 import {
     CanvasWidget
 } from '@projectstorm/react-canvas-core';
+import { useState, useEffect } from 'react';
 
 
 const Architecture = () => {
     const engine = createEngine();
-
+    const [links, setLinks] = useState([])
     const node1 = new DefaultNodeModel({
         name: 'Source',
         color: 'rgb(0,255,0)',
@@ -19,6 +20,12 @@ const Architecture = () => {
     node1.setPosition(100, 100);
     let port1 = node1.addOutPort(' ');
 
+    // node1.addListener({
+    //     selectionChanged: () => { console.log("selectionChanged") }
+    // });
+    // node1.addListener("drag", function (event) {
+    //     console.log("source node is dragged");
+    // })
     const node2 = new DefaultNodeModel({
         name: 'Destination',
         color: 'rgb(255,0,0)',
@@ -26,11 +33,25 @@ const Architecture = () => {
     node2.setPosition(400, 100);
     let port2 = node2.addInPort(' ');
 
+    // node2.addListener({
+    //     selectionChanged: () => { console.log("selectionChanged") }
+    // });
+    // node2.addListener("drag", function (event) {
+    //     console.log("destination node is dragged");
+    // })
     const link = port1.link < DefaultLinkModel > (port2);
-    console.log(link);
-
 
     const model = new DiagramModel();
+    model.registerListener({
+        nodesUpdated: function (e) {
+            console.log("Nodes Updated")
+        },
+        linksUpdated: function (e) {
+            console.log(e.link)
+            // const linkJSON = { src: e.link.sourcePort.options.id, dst: e.link.targetPort.options.id }
+            // console.log(linkJson);
+        },
+    });
     model.addAll(node1, node2, link);
     engine.setModel(model);
 
--- app-ui/src/index.js
+++ app-ui/src/index.js
@@ -2,6 +2,36 @@ import React from "react"
 import ReactDOM from "react-dom"
 import BasicConnection from './BasicConnection'
 
+// let response = await fetch("localhost: 3000 / api / state / cache", {
+//   method: 'POST', // *GET, POST, PUT, DELETE, etc.
+
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+
+//   body: JSON.stringify({
+//     "components": [
+//       {
+//         "id": "c1",      // unique identifier for first box created
+//         "name": "Source", // name of the box/component
+//       },
+//       {
+//         "id": "c2",
+//         "name": "Destination"
+//       }
+//     ],
+//     "links": [
+//       {
+//         "src": "c1",    // source of the link
+//         "dest": "c2"    // destination
+//       }
+//     ]
+//   }) // body data type must match "Content-Type" header
+// });
+// response = await response.json();
+// console.log(response);
+
+
 ReactDOM.render(
   <React.StrictMode>
     <BasicConnection />
